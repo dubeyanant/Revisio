c07a8a57424437acecc637a66e1320994a7c2751
dubeyanant <anantkumar312@gmail.com>
2023-08-26 17:34:23 +0530

    feat: dynamic twitter url fetch

--- src/twitter.js
+++ src/twitter.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 // Add the Twitter widget script here
 const twitterWidgetScript = document.createElement("script");
@@ -8,6 +8,8 @@ twitterWidgetScript.charset = "utf-8";
 document.head.appendChild(twitterWidgetScript);
 
 function TweetEmbed() {
+  const [data, setData] = useState({});
+
   useEffect(() => {
     // Create a function to initialize the Twitter widget
     const initializeTwitterWidget = () => {
@@ -22,15 +24,56 @@ function TweetEmbed() {
 
     // Load the Twitter widget
     initializeTwitterWidget();
+
+    const fetchData = async () => {
+      try {
+        const rawResponse = await fetch("http://localhost:3000/url");
+
+        // Check if the rawResponse status code indicates success (e.g., 200 OK)
+        if (!rawResponse.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        // Parse the rawResponse body as JSON
+        const response = await rawResponse.json();
+
+        const videoEmbedArray = response.map((video) => {
+          const url = video.url;
+          const parsedUrl = new URL(url);
+
+          // Check if the hostname is "www.youtube.com"
+          if (parsedUrl.hostname === "twitter.com") return url;
+          return null;
+        });
+
+        // Filter out null values to remove empty URLs
+        const VideoEmbedArray = videoEmbedArray.filter((url) => url !== null);
+        setData(VideoEmbedArray);
+
+        // Use the response as needed
+        console.log("Response from the API:", response);
+      } catch (error) {
+        console.error("Error fetching bata:", error);
+      }
+    };
+
+    // Call the fetchData function to make the GET request
+    fetchData();
   }, []);
 
-  return (
-    <div className="tweet-container">
-      <blockquote className="twitter-tweet">
-        <a href="https://twitter.com/_aanant/status/1695173009181659298"> </a>
-      </blockquote>
-    </div>
-  );
+  if (data.length > 0) {
+    return (
+      <div className="tweet-container">
+        {data.map((twitterUrl) => (
+          <blockquote className="twitter-tweet">
+            <a href={twitterUrl}> </a>
+          </blockquote>
+        ))}
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 export default TweetEmbed;
