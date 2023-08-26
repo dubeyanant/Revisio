19e12a45d97c647ec12bf51ecbaab2ffdec0dc20
dubeyanant <anantkumar312@gmail.com>
2023-08-26 13:32:08 +0530

    feat: show embed video

--- src/index.js
+++ src/index.js
@@ -1,43 +1,57 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-const fetchData = async () => {
-  try {
-    const response = await fetch("http://localhost:3001/url");
-
-    // Check if the response status code indicates success (e.g., 200 OK)
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    // Parse the response body as JSON
-    const data = await response.json();
-
-    // Use the data as needed
-    console.log("Data from the API:", data);
-  } catch (error) {
-    console.error("Error fetching bata:", error);
-  }
-};
-
-// Call the fetchData function to make the GET request
-fetchData();
-
-const youtubeUrl = "https://www.youtube.com/watch?v=XmGO4O2vpJw";
-const videoId = youtubeUrl.match(/[?&]v=([^&]+)/)[1];
-const youtubeVideoUrl = `https://www.youtube.com/embed/${videoId}`;
-
 function ContentList() {
-  return (
-    <iframe
-      width="560"
-      height="315"
-      src={youtubeVideoUrl}
-      title="YouTube Video"
-      frameBorder="0"
-      allowFullScreen
-    />
-  );
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const rawResponse = await fetch("http://localhost:3000/url");
+
+        // Check if the rawResponse status code indicates success (e.g., 200 OK)
+        if (!rawResponse.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        // Parse the rawResponse body as JSON
+        const response = await rawResponse.json();
+        const videoEmbedArray = response.map((video) => {
+          const url = video.url;
+          const videoId = url.match(/[?&]v=([^&]+)/)[1];
+          return `https://www.youtube.com/embed/${videoId}`;
+        });
+        setData(videoEmbedArray);
+
+        // Use the response as needed
+        console.log("Response from the API:", response);
+      } catch (error) {
+        console.error("Error fetching bata:", error);
+      }
+    };
+
+    // Call the fetchData function to make the GET request
+    fetchData();
+  }, []);
+
+  if (data.length > 0) {
+    return (
+      <>
+        {data.map((youtubeVideoUrl) => (
+          <iframe
+            width="560"
+            height="315"
+            key={youtubeVideoUrl}
+            src={youtubeVideoUrl}
+            title="YouTube Video"
+            frameBorder="0"
+            allowFullScreen
+          />
+        ))}
+      </>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
